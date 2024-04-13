b538338ef5a86778247fb45c28b5e3b630c9bf5f
Akshay Gupta <akshaygupta0795@gmail.com>
2024-04-13 16:25:12 +0530

    [main] make event listener for scroll passive

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,24 +1,28 @@
 export const activeSection = () => {
-  window?.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.pp-section');
-    const navLi = document.querySelectorAll('.nav-menu li');
-    let current = '';
-    sections.forEach((section) => {
-      const sectionTop = (section as HTMLElement).offsetTop;
-      const sectionHeight = (section as HTMLElement).clientHeight;
-      if (scrollY >= sectionTop - sectionHeight / 3) {
-        current = section.getAttribute('id') || '';
-      }
-    });
-    navLi.forEach((li) => {
-      li.classList.remove('active');
-      if (
-        li.getElementsByTagName('a')[0].getAttribute('href') == `#${current}`
-      ) {
-        li.classList.add('active');
-      }
-    });
-  });
+  window?.addEventListener(
+    'scroll',
+    () => {
+      const sections = document.querySelectorAll('.pp-section');
+      const navLi = document.querySelectorAll('.nav-menu li');
+      let current = '';
+      sections.forEach((section) => {
+        const sectionTop = (section as HTMLElement).offsetTop;
+        const sectionHeight = (section as HTMLElement).clientHeight;
+        if (scrollY >= sectionTop - sectionHeight / 3) {
+          current = section.getAttribute('id') || '';
+        }
+      });
+      navLi.forEach((li) => {
+        li.classList.remove('active');
+        if (
+          li.getElementsByTagName('a')[0].getAttribute('href') == `#${current}`
+        ) {
+          li.classList.add('active');
+        }
+      });
+    },
+    { passive: true }
+  );
 };
 
 export const getSkillsArray = () => [
