b9f352bae7accb93cb4a6eb504fdb48052c8f79f
Akshay Gupta <akshaygupta0795@gmail.com>
2024-10-30 17:46:39 +0530

    [fix] open links in new tab

--- src/app/components/SingleBlog/SingleBlog.tsx
+++ src/app/components/SingleBlog/SingleBlog.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import BlogImage from '@/app/components/BlogImage';
+// @ts-ignore
 import { PortableText } from '@portabletext/react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
@@ -7,6 +9,7 @@ import { Blog } from '@/sanity/types/blog';
 const CodeComponent = ({ value }: any) => {
   return (
     <div>
+      {/* @ts-ignore */}
       <SyntaxHighlighter language={value.language} style={dracula}>
         {value.code}
       </SyntaxHighlighter>
@@ -18,6 +21,35 @@ const components = {
   types: {
     image: BlogImage,
     code: CodeComponent,
+  },
+  marks: {
+    internalLink: ({
+      value,
+      children,
+    }: {
+      value: { slug: { current: string } };
+      children: React.ReactNode;
+    }) => {
+      const { slug = { current: '' } } = value;
+      const href = `/${slug.current}`;
+      return <Link href={href}>{children}</Link>;
+    },
+    link: ({
+      value,
+      children,
+    }: {
+      value: { blank?: boolean; href: string };
+      children: React.ReactNode;
+    }) => {
+      const { blank, href } = value;
+      return blank ? (
+        <Link href={href} target='_blank' rel='noopener noreferrer'>
+          {children}
+        </Link>
+      ) : (
+        <Link href={href}>{children}</Link>
+      );
+    },
   },
 };
 
