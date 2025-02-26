682be499cd2b0a9859a56555144e13b0f8d1a8b1
Akshay Gupta <akshay@peoplegrove.com>
2025-02-26 17:56:20 +0530

    [feat] remove skip-link

--- src/app/components/Layout/index.tsx
+++ src/app/components/Layout/index.tsx
@@ -12,9 +12,6 @@ const Layout = ({
 }) => {
   return (
     <Fragment>
-      <a href='#main-content' className='skip-link'>
-        Skip to main content
-      </a>
       <Header />
       <main
         id='main-content'
